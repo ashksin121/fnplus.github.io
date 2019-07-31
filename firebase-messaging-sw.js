2d349f34eb703de32a52eeae477be31c0939bf29
Uniyal <abhishekuniyal09@gmail.com>
2019-07-31 22:44:19 +0530

    Update firebase messaging and meta tags

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,13 +2,13 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts("https://www.gstatic.com/firebasejs/5.0.4/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/5.0.4/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/6.3.3/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/6.3.3/firebase-messaging.js");
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': '437825275503'
+  'messagingSenderId': '38226611639'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+<script>
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBL6LDmX6fuIy5d35iq15jz9fW-AnwtwDI",
+    authDomain: "community-updates.firebaseapp.com",
+    databaseURL: "https://community-updates.firebaseio.com",
+    projectId: "community-updates",
+    storageBucket: "community-updates.appspot.com",
+    messagingSenderId: "38226611639",
+    appId: "1:38226611639:web:f4eb0e1549522cc9"
+};
+firebase.initializeApp(config);
+</script>
