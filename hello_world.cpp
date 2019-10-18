63b1e26889d2d2aec154860048373eb43b0bb96b
Ashish Kirti Singh <ashksin121@gmail.com>
2019-10-19 00:51:38 +0530

    HelloWorld added

---
+++ hello_world.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define MOD 1000000007
+#define GREAT 1e12
+#define deb(x) cout<<#x<<" "<<x<<endl;
+ 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=GREAT;
+    // cin>>t;
+    while(t--) {
+        cout<<"Hello World\n";
+    }
+    return 0;
+}
